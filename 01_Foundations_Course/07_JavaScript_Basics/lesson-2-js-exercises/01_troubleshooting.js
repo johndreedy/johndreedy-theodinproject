80b9598e2a4f2807e4293e3564dcbb869e0305fa
John Reedy <johnreedy01@gmail.com>
2023-08-20 15:17:54 +0100

    Complete exercises for JS fundamentals part 2

---
+++ 01_Foundations_Course/07_JavaScript_Basics/lesson-2-js-exercises/01_troubleshooting.js
@@ -0,0 +1,28 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	// Edit between these lines
+	// =================================
+	result = a + b;
+
+        // johndreedy comments: just needed to change "a" + "b" to a + b with no quotations, so far rather easy
+
+	// =================================
+
+	return result;
+}
+
+
+
+// Do not change this 
+module.exports = troubleshooting;
